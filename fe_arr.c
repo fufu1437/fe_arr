35619342264ab2a7ccfe09478b1fb0702ba1805c
fufu1437 <128881350+fufu1437@users.noreply.github.com>
2026-03-07 23:43:33 +0800

    修改所有标识符的名称

--- fe_arr.c
+++ fe_arr.c
@@ -18,8 +18,8 @@ typedef struct FU_ARR{
     size_t used; //已储存元素数
 }FU_ARR;
 
-FU_ARR *fuarr_creat_arr(size_t s) {
-// FU_ARR* fuarr_creat_arr(size_t s, size_t type_size);
+FU_ARR *fearr_creat_arr(size_t s) {
+// FU_ARR* fearr_creat_arr(size_t s, size_t type_size);
     FU_ARR* arr = malloc(sizeof(FU_ARR));
     if(arr==NULL) return NULL;
     arr->value = calloc(s , sizeof(void*));
@@ -30,14 +30,14 @@ FU_ARR *fuarr_creat_arr(size_t s) {
     return arr;
 }
 
-void *fuarr_get(FU_ARR *arr, ArrInt index) {
+void *fearr_get(FU_ARR *arr, ArrInt index) {
     if( (index>=arr->used)||(arr==NULL)  ) {
         return NULL;
     }
     return arr->value[index];
 }
 
-int fuarr_put(FU_ARR *arr, void* v) {
+int fearr_put(FU_ARR *arr, void* v) {
     if(arr==NULL) return -1;
     if(arr->used>=arr->size) {
         void** temp = realloc(arr->value,(arr->size*bate) * sizeof(void*));
@@ -52,7 +52,7 @@ int fuarr_put(FU_ARR *arr, void* v) {
     return 0;
 }
 
-int fuarr_set(FU_ARR *arr, ArrInt index, void* v) {
+int fearr_set(FU_ARR *arr, ArrInt index, void* v) {
     if(arr==NULL) return -1;
     if(index >= arr->used) return -1;
     if(arr->used>=arr->size) {
@@ -70,7 +70,7 @@ int fuarr_set(FU_ARR *arr, ArrInt index, void* v) {
     return 0;
 }
 
-void *fuarr_pop(FU_ARR *arr) {
+void *fearr_pop(FU_ARR *arr) {
     if(arr==NULL) return NULL;
     arr->used--;
     void *temp = arr->value[arr->used];
@@ -78,7 +78,7 @@ void *fuarr_pop(FU_ARR *arr) {
     return temp;
 }
 
-void *fuarr_del(FU_ARR *arr, ArrInt index) {
+void *fearr_del(FU_ARR *arr, ArrInt index) {
     if(arr==NULL) return NULL;
     void *temp;
     arr->used--;
@@ -97,24 +97,24 @@ void *fuarr_del(FU_ARR *arr, ArrInt index) {
     return temp;
 }
 
-ArrInt fuarr_len(FU_ARR *arr) {
+ArrInt fearr_len(FU_ARR *arr) {
     if(arr==NULL) return -1;
     return arr->used;
 }
 
-ArrInt fuarr_size(FU_ARR *arr) {
+ArrInt fearr_size(FU_ARR *arr) {
     if(arr==NULL) return -1;
     return arr->size;
 }
 
-void fuarr_free_container(FU_ARR *arr) {
+void fearr_free_container(FU_ARR *arr) {
     if(arr==NULL) return;
     if(arr->value==NULL) { free(arr); return; }
     free(arr->value);
     free(arr);
 }
 
-void fuarr_free_all(FU_ARR *arr) {
+void fearr_free_all(FU_ARR *arr) {
     if(arr==NULL) return;
     if(arr->value==NULL) { free(arr); return; }
     for(ArrInt i=0;i<arr->used;i++) {
--- fe_arr.h
+++ fe_arr.h
@@ -1,5 +1,5 @@
-#ifndef FU_arr__fu_arr__fufu1437_h
-#define FU_arr__fu_arr__fufu1437_h
+#ifndef FE_arr__fe_arr__fefe1437_h
+#define FE_arr__fe_arr__fefe1437_h
 
 /*
  * 这是一个极其简单的动态列表
@@ -13,38 +13,38 @@
 // 可自行修改ArrInt的类型，以适应不同环境的需求
 typedef long ArrInt;
 
-typedef struct FU_ARR FU_ARR;
+typedef struct FE_ARR FE_ARR;
 
-FU_ARR* fuarr_creat_arr(size_t s);
-// FU_ARR* fuarr_creat_arr(size_t s, size_t type_size);
+FE_ARR* fearr_creat_arr(size_t s);
+// FE_ARR* fearr_creat_arr(size_t s, size_t type_size);
 
 // 获取 index 处的指针，超出数组长度返回NULL
-void *fuarr_get(FU_ARR *arr, ArrInt index);
+void *fearr_get(FE_ARR *arr, ArrInt index);
 
 // 向数组追加一个指针
-int fuarr_put(FU_ARR *arr, void *v);
+int fearr_put(FE_ARR *arr, void *v);
 
 // 设置 index 处的指针
 // 只允许对现有数据进行修改
 // 如超出数组长度，或 arr 是空指针返回NULL
-int fuarr_set(FU_ARR *arr, ArrInt index, void *v);
+int fearr_set(FE_ARR *arr, ArrInt index, void *v);
 
 // 返回数组长度
-ArrInt fuarr_len(FU_ARR *arr);
+ArrInt fearr_len(FE_ARR *arr);
 
 // 返回数组大小
-ArrInt fuarr_size(FU_ARR *arr);
+ArrInt fearr_size(FE_ARR *arr);
 
 // 弹出数组末尾的指针，并返回它，如果数组长度为0，或arr为空指针，则返回NULL
-void *fuarr_pop(FU_ARR *arr);
+void *fearr_pop(FE_ARR *arr);
 
 // 删除 index 处的指针，超出数组长度，或 arr 是空指针返回NULL
-void *fuarr_del(FU_ARR *arr, ArrInt index);
+void *fearr_del(FE_ARR *arr, ArrInt index);
 
 // 删除arr这个数组容器，其内部指针如果使用malloc分配，则需自行释放
-void fuarr_free_container(FU_ARR *arr);
+void fearr_free_container(FE_ARR *arr);
 
 // 删除arr这个容器，和数组的指针，仅适用于所有值均为malloc/relloc/calloc分配的内存的数组
-void fuarr_free_all(FU_ARR *arr);
+void fearr_free_all(FE_ARR *arr);
 
 #endif
